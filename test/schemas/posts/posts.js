21191cbb7a0e8431508026090c86565ae9b9d449
Stefania Silva <stefaniadasilva27@gmail.com>
2022-09-18 19:17:16 -0300

    first commit

---
+++ test/schemas/posts/posts.js
@@ -0,0 +1,43 @@
+const Joi = require("joi");
+const supertest = require("supertest");
+
+const request = supertest("https://jsonplaceholder.typicode.com");
+
+describe('GET /posts', () => {
+    it('Deve retornar uma lista de posts', done => {
+        const postsList = Joi.array().items(Joi.object().keys({
+            userId: Joi.number(),
+            id: Joi.number(),
+            title: Joi.string(),
+            body: Joi.string()
+        }));
+
+        request
+        .get("/posts")
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end((err, res) => {
+            Joi.assert(res.body, postsList);
+            done(err);
+        });
+
+    });
+
+    it('Deve validar o schema de um posts único', done => {
+        const postsList = Joi.object().keys({
+          userId: Joi.number(),
+          id: Joi.number(),
+          title: Joi.string(),
+          body: Joi.string()
+        });
+    
+        request
+          .get("/posts/1")
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .end((err, res) => {
+            Joi.assert(res.body, postsList);
+            done(err);
+          });
+      });
+});
---
+++ test/schemas/users/users.js
@@ -0,0 +1,41 @@
+const Joi = require("joi");
+const supertest = require("supertest");
+
+const request = supertest("https://jsonplaceholder.typicode.com");
+
+describe('GET /users', () => {
+    it('Deve validar o schema de uma lista de usuários', done => {
+      const userList = Joi.array().items(Joi.object().keys({
+        id: Joi.number(),
+        name: Joi.string(),
+        username: Joi.string(),
+        email: Joi.string(),
+        address: Joi.object().keys({
+          street: Joi.string(),
+          suite: Joi.string(),
+          city: Joi.string(),
+          zipcode: Joi.string(),
+          geo: Joi.object().keys({
+            lat: Joi.string(),
+            lng: Joi.string() 
+          })
+        }),
+        phone: Joi.string(),
+        website: Joi.string(),
+        company: Joi.object().keys({
+          name: Joi.string(),
+          catchPhrase: Joi.string(),
+          bs: Joi.string()
+        })
+      }));
+  
+      request
+        .get("/users")
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end((err, res) => {
+          Joi.assert(res.body, userList);
+          done(err);
+        });
+    });
+  });
